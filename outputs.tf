1166b1f40e95d5604307f776d4f819a5a618bd81
sisipka <96078556+sisipka@users.noreply.github.com>
2023-04-05 21:44:06 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,6 @@ output "kuber-shestihin_external_v4_address" {
   value       = yandex_kubernetes_cluster.kuber-shestihin.master[0].external_v4_endpoint
 }
 
-
 output "cluster_ca_certificate" {
   description = <<-EOF
   PEM-encoded public certificate that is the root of trust for
@@ -11,7 +10,6 @@ output "cluster_ca_certificate" {
 
   value = yandex_kubernetes_cluster.kuber-shestihin.master[0].cluster_ca_certificate
 }
-
 
 output "cluster_id" {
   description = "ID of a new Kubernetes cluster."
